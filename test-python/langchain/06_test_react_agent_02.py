39ddb3bd23fda65a59086ee7b62a54a2bf7deab0
Paul Wong <paulwong888@163.com>
2024-08-11 23:57:19 +0800

    add gen video script

--- test-python/langchain/06_test_react_agent_02.py
+++ test-python/langchain/06_test_react_agent_02.py
@@ -20,7 +20,8 @@ def check_time(format: str = "%Y-%m-%d %H:%M:%S"):
     return formatted_time
 
 tools = [check_time, image_caption]
-tools.extend(load_tools(tool_names=["wikipedia"]))
+tools.extend(load_tools(tool_names=["wikipedia", "serpapi"]))
+tools.extend(load_tools(tool_names=["llm-math"], llm=llm))
 
 agent = create_react_agent(
     llm=llm,
@@ -41,5 +42,6 @@ agent_executor = AgentExecutor(
 
 # img_url = "https://ix-www.imgix.net/case-study/unsplash/woman-hat.jpg?ixlib=js-3.8.0&w=400&auto=compress%2Cformat&dpr=1&q=75"
 img_url = "file:///Users/paulwong/PAUL/WORKS/WORKSPACES/TEST-WORKSPACE/python-ai/test-python/langchain/woman-hat.jpg"
-result = agent_executor.invoke({"input":f"这张图片展示了什么？\n {img_url}"})
+# result = agent_executor.invoke({"input":f"这张图片展示了什么？\n {img_url}"})
+result = agent_executor.invoke({"input":f"香港今天的温度是多少度？温度用摄氏表示，比深圳的温度高多少？"})
 print(result)
---
+++ test-python/video_script_generator/main.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from dotenv import load_dotenv
+from utils import generate_script
+import os
+
+load_dotenv()
+
+st.title("视频脚本生成器")
+
+with st.sidebar:
+    openai_api_key = st.text_input("请输入OpenAi API密钥：", type="password", value=os.getenv("OPENAI_API_KEY"))
+    st.markdown("[获取OpenAi API密钥](https://platform.openai.com/account/api-key)")
+
+subject = st.text_input("请输入视频的主题", value="sora模型")
+video_length = st.number_input("请输入视频的大致时长(单位：分钟)", min_value=0.1, step=0.1, value=1.0)
+creativity = st.slider(
+    "请输入视频脚本的创造力(数字小说明更严重，数字大说明更多样)",
+    min_value=0.1, max_value=1.0, value=0.2, step=0.1
+)
+submit = st.button("生成脚本")
+
+if submit:
+    if not openai_api_key:
+        st.info("请输入OpenAi API密钥")
+        st.stop()
+    if not subject:
+        st.info("请输入视频的主题")
+        st.stop()
+    if not video_length >= 0.1:
+        st.info("视频的长度需要大于或等于0.1")
+        st.stop()
+
+    with st.spinner(("AI正在思考中，请稍等...")):
+        search_result, title, script = generate_script(subject, video_length, creativity, openai_api_key)
+    st.success("视频脚本已生成！")
+    st.subheader("标题： ")
+    st.write(title)
+    st.subheader("视频脚本： ")
+    st.write(script)
+
+    with st.expander("维基百科搜索结果 "):
+        st.info(search_result)
+
+    
