042013c99832698fb8d18cc8223a10286ef262f2
Paul Wong <paulwong888@163.com>
2024-08-12 16:36:39 +0800

    change to subheader

--- video_script_generator/main.py
+++ video_script_generator/main.py
@@ -5,7 +5,8 @@ import os
 
 load_dotenv()
 
-st.title("视频脚本生成器(Present by Paul Wong)")
+st.title("视频脚本生成器")
+st.subheader("Present by Paul Wong")
 
 with st.sidebar:
     openai_api_key = st.text_input("请输入OpenAi API密钥：", type="password")
