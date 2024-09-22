0e8fb68f04a3805118edeac9b3cffe0d890cbf76
paulwong888@163.com <paulwong888@163.com>
2024-09-22 22:55:47 +0800

    update

--- video_script_generator/main.py
+++ video_script_generator/main.py
@@ -20,16 +20,16 @@ st.title("🎬 視頻腳本生成器")
 st.subheader("Present by Paul Wong")
 
 with st.sidebar:
-    openai_api_key = st.text_input("请输入OpenAi API密钥：", type="password")
-    st.markdown("[获取OpenAi API密钥](https://platform.openai.com/account/api-key)")
+    openai_api_key = st.text_input("請輸入OpenAi API密钥：", type="password")
+    st.markdown("[獲取OpenAi API密钥](https://platform.openai.com/account/api-key)")
 
-subject = st.text_input("💡 请输入视频的主题", value="sora模型")
-video_length = st.number_input("⏱️ 请输入视频的大致时长(单位：分钟)", min_value=0.1, step=0.1, value=1.0)
+subject = st.text_input("💡 請輸入視頻的主題", value="sora模型")
+video_length = st.number_input("⏱️ 請輸入視頻的大致時長(組織：分鐘)", min_value=0.1, step=0.1, value=1.0)
 creativity = st.slider(
-    "✨ 请输入视频脚本的创造力(数字小说明更严重，数字大说明更多样)",
+    "✨ 請輸入視頻腳本的創造力(數位小說明更嚴重，數位大說明更多樣)",
     min_value=0.1, max_value=1.0, value=0.2, step=0.1
 )
-submit = st.button("生成脚本")
+submit = st.button("生成腳本")
 
 if submit:
     if not openai_api_key:
@@ -37,21 +37,21 @@ if submit:
         # st.info("请输入OpenAi API密钥")
         # st.stop()
     if not subject:
-        st.info("请输入视频的主题")
+        st.info("請輸入視頻的主題")
         st.stop()
     if not video_length >= 0.1:
-        st.info("视频的长度需要大于或等于0.1")
+        st.info("視頻的長度需要大於或等於0.1")
         st.stop()
 
-    with st.spinner(("AI正在思考中，请稍等...")):
+    with st.spinner(("AI正在思考中，請稍等...")):
         search_result, title, script = generate_script(subject, video_length, creativity, openai_api_key)
-    st.success("视频脚本已生成！")
+    st.success("視頻腳本已生成！")
     st.subheader("🔥 标题： ")
     st.write(title)
-    st.subheader("📝 视频脚本： ")
+    st.subheader("📝 視頻腳本： ")
     st.write(script)
 
-    with st.expander("维基百科搜索结果 👀"):
+    with st.expander("維基百科搜索結果 👀"):
         st.info(search_result)
 
     
