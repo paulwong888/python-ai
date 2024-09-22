366c7615203989af885fa595f6f7149bbffbeec6
paulwong888@163.com <paulwong888@163.com>
2024-09-22 23:04:04 +0800

    update

--- xiaohongshu_generator/main.py
+++ xiaohongshu_generator/main.py
@@ -2,40 +2,26 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 from utils import generate_xiaohongshu
+from commons.sidebar import display_sidebar, remove_button
 
-from utils import generate_xiaohongshu
-
+openai_api_key = display_sidebar()
+remove_button()
 load_dotenv()
-# Add custom CSS to hide the GitHub icon
-st.markdown(
-    r"""
-    <style>
-    #MainMenu {visibility: hidden;}
-    [data-testid="stActionButton"] {visibility: hidden;}
-    footer {visibility: hidden;}
-    </style>
-    """, 
-    unsafe_allow_html=True
-)
 
-st.title("爆款小红书AI写作助手 ✏️")
+st.title("爆款小紅書AI寫作助手 ✏️")
 st.subheader("Present by Paul Wong")
 
-with st.sidebar:
-    openai_api_key = st.text_input("请输入OpenAI API密钥：", type="password")
-    st.markdown("[获取OpenAI API密钥](https://platform.openai.com/account/api-key)")
-
-topic = st.text_input("主题", value="大模型")
-submit = st.button("开始写作")
+topic = st.text_input("主題", value="大模型")
+submit = st.button("開始寫作")
 
 if submit:
     if not openai_api_key:
         openai_api_key = os.getenv("OPENAI_API_KEY")
     if not topic:
-        st.info("请输入主题")
+        st.info("請輸入主題")
         st.stop()
     
-    with st.spinner("AI正在努力创作中，请稍候..."):
+    with st.spinner("AI正在努力创作中，請稍候..."):
         xiaohongshu_instance = generate_xiaohongshu(topic, openai_api_key)
 
     st.divider()
@@ -44,9 +30,9 @@ if submit:
     with left_column:
         titles = xiaohongshu_instance.titles
         for i in range(len(titles)):
-            st.markdown(f"##### 小红书标题{i+1}")
+            st.markdown(f"##### 小紅書標題{i+1}")
             st.write(titles[i])
     with right_column:
-        st.markdown("##### 小红书正文")
+        st.markdown("##### 小紅書正文")
         st.write(xiaohongshu_instance.content)
 
