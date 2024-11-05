a6ab0423bbfecc80991f7e36fe3c341c7b600ef4
Paul Wong <paulwong888@163.com>
2024-11-05 11:10:53 +0800

    update

--- test-deeplearning/pure_pytorch/06_classify_simple.py
+++ test-deeplearning/pure_pytorch/06_classify_simple.py
@@ -6,13 +6,11 @@ from sklearn import datasets
 from torch.utils.data import TensorDataset, DataLoader
 
 class Classify(nn.Module):
-    def __init__(self, input_size, output_size, X_numpy, y_numpy):
+    def __init__(self, input_size, output_size, X, y):
         super().__init__()
         self.linear = nn.Linear(in_features=input_size, out_features=output_size)
-        self.X_numpy = X_numpy
-        self.y_numpy = y_numpy
-        self.X = torch.Tensor(X_numpy)
-        self.y = torch.Tensor(y_numpy.reshape(100, 1))
+        self.X = X
+        self.y = y
     
     def forward(self, X_part):
         pred_y = torch.sigmoid(self.linear(X_part))
@@ -26,7 +24,7 @@ class Classify(nn.Module):
     
     def show_fit(self, title):
         plt.title(title)
-        X_temp = self.X_numpy
+        X_temp = self.X.numpy()
         y_temp = self.y.view(100).numpy()
         # X[y==0, 0] 返回的是一个一维数组，包含了所有属于第一个簇（标签为0）的数据点的x坐标
         plt.scatter(X_temp[y_temp==0, 0], X_temp[y_temp==0, 1])
@@ -67,34 +65,38 @@ class Classify(nn.Module):
         else:
             return 0
 
-n_pts = 100
+def train_test():
+    n_pts = 100
 
-"""
-使用make_blobs函数生成数据集，X 是二维数组，y是一维数组
-设置参数：n_pts 表示生成的数据点数量为100，
-centers 表示数据点的中心位置，这里有两个中心点，分别在(-0.5, 0.5)和(0.5, -0.5)。
-random_state参数设置为123以确保结果的可重复性，
-cluster_std参数设置为0.4，表示每个簇的标准差。
-"""
-centers = [[-0.5, 0.5], [0.5, -0.5]]
-(X_numpy, y_numpy) = datasets.make_blobs(n_samples=n_pts, random_state=123, centers=centers, cluster_std=0.4)
-print(X_numpy, y_numpy)
+    """
+    使用make_blobs函数生成数据集，X 是二维数组，y是一维数组
+    设置参数：n_pts 表示生成的数据点数量为100，
+    centers 表示数据点的中心位置，这里有两个中心点，分别在(-0.5, 0.5)和(0.5, -0.5)。
+    random_state参数设置为123以确保结果的可重复性，
+    cluster_std参数设置为0.4，表示每个簇的标准差。
+    """
+    centers = [[-0.5, 0.5], [0.5, -0.5]]
+    (X_numpy, y_numpy) = datasets.make_blobs(n_samples=n_pts, random_state=123, centers=centers, cluster_std=0.4)
+    print(X_numpy, y_numpy)
 
-X = torch.Tensor(X_numpy)
-y = torch.Tensor(y_numpy.reshape(100, 1))
-model = Classify(2,1, X_numpy, y_numpy)
-model.show_fit("Initial Model")
+    X = torch.Tensor(X_numpy)
+    y = torch.Tensor(y_numpy.reshape(100, 1))
+    model = Classify(2,1, X, y)
+    model.show_fit("Initial Model")
 
-model.train(1000, 100)
-model.show_fit("Trained Model")
+    model.train(1000, 100)
+    model.show_fit("Trained Model")
 
-point1 = torch.tensor([1.0, -1.0])
-point2 = torch.tensor([-1.0, 1.0])
-print(f"Red point positive probalility = {model.forward(point1).item()}")
-print(f"Black point positive probalility = {model.forward(point2).item()}")
-print(f"Red point in class {model.predit(point1)}")
-print(f"Black point in class {model.predit(point2)}")
+    point1 = torch.tensor([1.0, -1.0])
+    point2 = torch.tensor([-1.0, 1.0])
+    print(f"Red point positive probalility = {model.forward(point1).item()}")
+    print(f"Black point positive probalility = {model.forward(point2).item()}")
+    print(f"Red point in class {model.predit(point1)}")
+    print(f"Black point in class {model.predit(point2)}")
 
-plt.plot(point1.numpy()[0], point1.numpy()[1], "ro")
-plt.plot(point2.numpy()[0], point2.numpy()[1], "ko")
-model.show_fit("Trained Model")
+    plt.plot(point1.numpy()[0], point1.numpy()[1], "ro")
+    plt.plot(point2.numpy()[0], point2.numpy()[1], "ko")
+    model.show_fit("Trained Model")
+
+if __name__ == "__main__":
+    train_test()
